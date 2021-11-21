bf14e7d413c925f1ff491f9241b2f8a90a7441b5
Joel Cabana <joelxcabana.com>
2021-11-21 11:23:20 -0300

    obtener la imagen del producto y generar el documento excel con la informacion obtenida de los productos

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const ExcelJS = require('exceljs')
 const randomUseragent = require('random-useragent')
 
 const initialization = async() => {
@@ -24,6 +25,8 @@ const initialization = async() => {
 
    //$$ significa que se repite el elemento
    const listaItems = await page.$$('.ui-search-layout__item')
+
+   let data = []
    for(const item of listaItems){
 
     //sacar el elemento renderizado
@@ -35,7 +38,18 @@ const initialization = async() => {
      const objectoNombre = await item.$('.ui-search-item__title')
      //del elemento sacar el texto
      const getName = await page.evaluate(objectoNombre => objectoNombre.innerText, objectoNombre)
-     console.log(`${getPrice} - ${getName}`)
+
+     //obtener imagen ingresar a atributo
+     const image = await item.$('.ui-search-result-image__element')
+    const getImage = await page.evaluate(image => image.getAttribute('src'), image)
+
+     console.log(`${getPrice} -- ${getName} -- ${getImage}`)
+
+     data.push({
+         name:getName,
+         price:getPrice,
+         image:getImage
+     })
    }
    
 
@@ -43,7 +57,35 @@ const initialization = async() => {
    //await page.screenshot({path:'example.png'})
 
    //cerrar el navegador
-  // await browser.close()
+   await browser.close()
+
+   saveExcel(data)
 }
+
+const saveExcel = (data) =>{
+
+    const workbook = new ExcelJS.Workbook()
+    const filename = 'test.xlsx'
+
+    const sheet = workbook.addWorksheet('Result')
+
+    const reColums = [
+        {header :'Nombre',key:'name'},
+        {header :'Precio',key:'price'},
+        {header :'Imagen',key:'image'}
+    ]
+
+    sheet.columns = reColums
+
+    sheet.addRow(data)
+
+    workbook.xlsx.writeFile(filename).then((e) =>{
+        console.log('creado exitosamente')
+    })
+    .catch(()=>{
+        console.log("error al crear")
+    })
+}
+
 
 initialization()
